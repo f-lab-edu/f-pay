39c7b1e76af29f19fe43622ebdb0bd02d88391a3
sungjunjin <jinsungjun94@gmail.com>
2024-04-12 18:46:31 +0900

    feat: not supported method 예외 핸들링 추가

--- src/main/java/com/flab/fpay/exception/GlobalExceptionHandler.java
+++ src/main/java/com/flab/fpay/exception/GlobalExceptionHandler.java
@@ -6,6 +6,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -20,6 +22,13 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(exception.getErrorCode().getStatus())
                 .body(new FailResponse(exception.getMessage()));
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<FailResponse> handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException exception) {
+        return ResponseEntity
+                .status(HttpStatus.METHOD_NOT_ALLOWED)
+                .body(new FailResponse("method not allowed"));
     }
 
     private void logMessage(ErrorCode errorCode, Exception exception) {
