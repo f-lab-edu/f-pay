18e4c822b1da0d06ecaf469be684786797bae0bc
sungjun <jinsungjun94@gmail.com>
2024-04-15 15:06:34 +0900

    feat: service, repository 로직 작성

--- src/main/java/com/flab/fpay/service/member/MemberService.java
+++ src/main/java/com/flab/fpay/service/member/MemberService.java
@@ -1,4 +1,10 @@
 package com.flab.fpay.service.member;
 
+import com.flab.fpay.domain.member.dto.MemberRegisterDto;
+import com.flab.fpay.domain.member.entity.Member;
+
 public interface MemberService {
+    void registerMember(MemberRegisterDto memberRegisterDto);
+
+    Member getMemberByEmail(String email);
 }
